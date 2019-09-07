f7ccd278f417377dacc3afaa78db59f616fab722
Ironbatpug <molnarcsaba92@gmail.com>
2019-09-07 17:06:14 +0200

    lesson6 keyboard

---
+++ breakpoint/Extensions/UIViewExt.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewExt.swift
+//  breakpoint
+//
+//  Created by Molnár Csaba on 2019. 09. 07..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardwillChage(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc func keyboardwillChage(_ notification: NSNotification){
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let beginFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endingFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endingFrame.origin.y - beginFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+        }, completion: nil)
+    }
+}
