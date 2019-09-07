c0b27f801f0537a87bc7957195f5050d51716ad0
Ironbatpug <molnarcsaba92@gmail.com>
2019-09-07 15:28:48 +0200

    lesson5 mevc

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,25 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Molnár Csaba on 2019. 09. 07..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logoutBtnWasPressed(_ sender: Any) {
+    }
+}
