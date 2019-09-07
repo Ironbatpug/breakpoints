66cb419a3a65437ba9d924909fa76f71a59e3bb9
Ironbatpug <molnarcsaba92@gmail.com>
2019-09-07 13:32:01 +0200

    lesson2 views added

---
+++ breakpoint/View/ShadowView.swift
@@ -0,0 +1,19 @@
+//
+//  ShadowView.swift
+//  breakpoint
+//
+//  Created by Molnár Csaba on 2019. 09. 07..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        self.layer.shadowOpacity = 0.75
+        self.layer.shadowRadius = 5
+        self.layer.shadowColor = UIColor.black.cgColor
+        super.awakeFromNib()
+    }
+}
